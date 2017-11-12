cd32ef6f9ea2928a8e9eecee80df3172c9dad173
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-12 16:42:20 -0600

    Making it possible to pass in a custom dataset.

--- index.js
+++ index.js
@@ -11,30 +11,39 @@ module.exports            = function(paperclip) {
     if (options.object_type == undefined) {
       options.object_type = 'FACE_CASCADE';
     }
-
+    if (options.dataset == undefined) {
+      options.dataset = cv[options.object_type];
+      options.custom  = false;
+    } else {
+      options.custom  = true;
+    }
 
     cv.readImage(this.paperclip.file().file.buffer, function(err, im){
-      im.detectObject(cv[options.object_type], {}, function(err, faces){
-        if (faces && faces.length > 0) {
-          switch(options.object_type) { 
-            case 'FACE_CASCADE':
-              next(err, {faces: {count: faces.length, data: faces}});
-              break;
-            case 'EYE_CASCADE':
-              next(err, {eyes: {count: faces.length, data: faces}});
-              break;
-            case 'EYEGLASSES_CASCADE':
-              next(err, {glasses: {count: faces.length, data: faces}});
-              break;
-            case 'FULLBODY_CASCADE':
-              next(err, {cars: {count: faces.length, data: faces}});       
-              break;
-            case 'CAR_SIDE_CASCADE':
-              next(err, {car_profiles: {count: faces.length, data: faces}});      
-              break;
-          }
+      im.detectObject(options.dataset, {}, function(err, faces){
+        if (options.custom == true) {
+          next(err, {objects: {count: faces.length, data: faces}})
         } else {
-          next(err, {});
+          if (faces && faces.length > 0) {
+            switch(options.object_type) { 
+              case 'FACE_CASCADE':
+                next(err, {faces: {count: faces.length, data: faces}});
+                break;
+              case 'EYE_CASCADE':
+                next(err, {eyes: {count: faces.length, data: faces}});
+                break;
+              case 'EYEGLASSES_CASCADE':
+                next(err, {glasses: {count: faces.length, data: faces}});
+                break;
+              case 'FULLBODY_CASCADE':
+                next(err, {cars: {count: faces.length, data: faces}});       
+                break;
+              case 'CAR_SIDE_CASCADE':
+                next(err, {car_profiles: {count: faces.length, data: faces}});      
+                break;
+            }
+          } else {
+            next(err, {});
+          }
         }
       });
     })
