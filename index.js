ca519df2ed88ff730b099c5604fc5ad00a2e3402
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-12 22:48:40 -0600

    Making it a little more customizable.

--- index.js
+++ index.js
@@ -16,29 +16,38 @@ module.exports            = function(paperclip) {
       options.custom  = false;
     } else {
       options.custom  = true;
+      if (options.attribute == undefined) {
+        options.attribute = 'objects';
+        console.log('');
+        console.log('NODE-PAPERCLIP-OPENCV-DETECTION:');
+        console.log('You can set the attribute option in the before_save task to save the information with a custom key.')
+        console.log('');
+      }
     }
 
     cv.readImage(this.paperclip.file().file.buffer, function(err, im){
-      im.detectObject(options.dataset, {}, function(err, faces){
+      im.detectObject(options.dataset, {}, function(err, objects){
         if (options.custom == true) {
-          next(err, {objects: {count: faces.length, data: faces}})
+          var object = {};
+	  object[options.attribute] = {count: objects.length, data: objects}
+          next(err, object);
         } else {
-          if (faces && faces.length > 0) {
+          if (objects && objects.length > 0) {
             switch(options.object_type) { 
               case 'FACE_CASCADE':
-                next(err, {faces: {count: faces.length, data: faces}});
+                next(err, {objects: {count: objects.length, data: faces}});
                 break;
               case 'EYE_CASCADE':
-                next(err, {eyes: {count: faces.length, data: faces}});
+                next(err, {eyes: {count: objects.length, data: objects}});
                 break;
               case 'EYEGLASSES_CASCADE':
-                next(err, {glasses: {count: faces.length, data: faces}});
+                next(err, {glasses: {count: objects.length, data: objects}});
                 break;
               case 'FULLBODY_CASCADE':
-                next(err, {cars: {count: faces.length, data: faces}});       
+                next(err, {cars: {count: objects.length, data: objects}});       
                 break;
               case 'CAR_SIDE_CASCADE':
-                next(err, {car_profiles: {count: faces.length, data: faces}});      
+                next(err, {car_profiles: {count: objects.length, data: objects}});      
                 break;
             }
           } else {
