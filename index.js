2f8fe788bf936e5eda1febc9e810955283738dbf
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-13 12:12:04 -0600

    better organization of options.  new version.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cv                  = require('opencv');
-
+const _                   = require('underscore');
 // This is mostly just an example showing how opencv can be integrated.  I think that
 // there must be some other really interesting applications of opencv and better ways 
 // of configuring if you have any suggestions, please feel from to make a pull request.
@@ -8,54 +8,64 @@ module.exports            = function(paperclip) {
   var obj = {};
   obj.paperclip           = paperclip;
   obj.perform             = function(options, next) {
-    if (options.object_type == undefined) {
-      options.object_type = 'FACE_CASCADE';
-    }
+
     if (options.dataset == undefined) {
-      options.dataset = cv[options.object_type];
-      options.custom  = false;
+      
+      if (options.type != undefined) {
+        
+        if (options.attribute == undefined) options.attribute = options.type;
+        
+        switch(options.type) { 
+          case 'faces':
+            _.extend(options, { dataset: cv.FACE_CASCADE });
+            break;
+          case 'eyes':
+            _.extend(options, { dataset: cv.EYE_CASCADE });
+            break;
+          case 'glasses':
+            _.extend(options, { dataset: cv.GLASSES_CASCADE });
+            break;
+          case 'cars':
+            _.extend(options, { dataset: cv.FULLBODY_CASCADE });
+            break;
+          case 'car_profiles':
+            _.extend(options, { dataset: cv.CAR_SIDE_CASCADE });
+            break;
+        } 
+
+      } else {
+        options.dataset = cv.FACE_CASCADE;
+        options.attribute = 'faces';
+      }
+
     } else {
-      options.custom  = true;
+      
       if (options.attribute == undefined) {
         options.attribute = 'objects';
-        console.log('');
-        console.log('NODE-PAPERCLIP-OPENCV-DETECTION:');
-        console.log('You can set the attribute option in the before_save task to save the information with a custom key.')
-        console.log('');
+        console.log('Set the options.attribute option to save it as something other than objects.');
       }
+
     }
 
     cv.readImage(this.paperclip.file().file.buffer, function(err, im){
       im.detectObject(options.dataset, {}, function(err, objects){
-        if (options.custom == true) {
-          var object = {};
-	  object[options.attribute] = {count: objects.length, data: objects}
+        var object = {};
+
+        object[options.attribute] = { count: objects.length, data: objects };
+        
+        if (objects.length > 0 || options.presence) {
+          if (objects.length > 0 && options.tag) {
+            object.object_tags = [options.attribute]
+          }
           next(err, object);
         } else {
-          if (objects && objects.length > 0) {
-            switch(options.object_type) { 
-              case 'FACE_CASCADE':
-                next(err, {objects: {count: objects.length, data: faces}});
-                break;
-              case 'EYE_CASCADE':
-                next(err, {eyes: {count: objects.length, data: objects}});
-                break;
-              case 'EYEGLASSES_CASCADE':
-                next(err, {glasses: {count: objects.length, data: objects}});
-                break;
-              case 'FULLBODY_CASCADE':
-                next(err, {cars: {count: objects.length, data: objects}});       
-                break;
-              case 'CAR_SIDE_CASCADE':
-                next(err, {car_profiles: {count: objects.length, data: objects}});      
-                break;
-            }
-          } else {
-            next(err, {});
-          }
+          next(err, {});
         }
+
       });
     })
   }
+
   return obj;
+
 }
