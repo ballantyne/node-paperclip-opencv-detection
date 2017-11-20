b6901a1bed926c0db20db52f3792d1a4b8ff8311
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-20 04:38:24 -0600

    wrapping this in a try catch block because it seems to fail and cause the upload to fail, I am not sure what is causing the failure, but it seems like it was an issue with opencv and the buffer.

--- index.js
+++ index.js
@@ -8,68 +8,71 @@ module.exports            = function(paperclip) {
   var obj = {};
   obj.paperclip           = paperclip;
   obj.perform             = function(options, next) {
+      if (options.dataset == undefined) {
 
-    if (options.dataset == undefined) {
-      
-      if (options.type != undefined) {
-        
-        if (options.attribute == undefined) options.attribute = options.type;
-        
-        switch(options.type) { 
-          case 'faces':
-            _.extend(options, { dataset: cv.FACE_CASCADE });
-            break;
-          case 'eyes':
-            _.extend(options, { dataset: cv.EYE_CASCADE });
-            break;
-          case 'glasses':
-            _.extend(options, { dataset: cv.GLASSES_CASCADE });
-            break;
-          case 'cars':
-            _.extend(options, { dataset: cv.FULLBODY_CASCADE });
-            break;
-          case 'car_profiles':
-            _.extend(options, { dataset: cv.CAR_SIDE_CASCADE });
-            break;
-        } 
+        if (options.type != undefined) {
 
-      } else {
-        options.dataset = cv.FACE_CASCADE;
-        options.attribute = 'faces';
-      }
+          if (options.attribute == undefined) options.attribute = options.type;
 
-    } else {
-      
-      if (options.attribute == undefined) {
-        options.attribute = 'objects';
-        console.log('Set the options.attribute option to save it as something other than objects.');
-      }
-
-    }
-
-    cv.readImage(this.paperclip.file().file.buffer, function(err, im){
-      im.detectObject(options.dataset, {}, function(err, objects){
-        var object = {};
-
-        
-        if (objects.length > 0 || options.presence) {
-          
-          object[options.attribute] = { count: objects.length, data: objects };
-
-          if (objects.length > 0 && options.tag) {
-            object.object_tags = [options.attribute]
-          }
-  
-          next(err, object);
+          switch(options.type) { 
+            case 'faces':
+              _.extend(options, { dataset: cv.FACE_CASCADE });
+              break;
+            case 'eyes':
+              _.extend(options, { dataset: cv.EYE_CASCADE });
+              break;
+            case 'glasses':
+              _.extend(options, { dataset: cv.GLASSES_CASCADE });
+              break;
+            case 'cars':
+              _.extend(options, { dataset: cv.FULLBODY_CASCADE });
+              break;
+            case 'car_profiles':
+              _.extend(options, { dataset: cv.CAR_SIDE_CASCADE });
+              break;
+          } 
 
         } else {
-        
-          next(err, {});
-        
+          options.dataset = cv.FACE_CASCADE;
+          options.attribute = 'faces';
         }
 
-      });
-    })
+      } else {
+
+        if (options.attribute == undefined) {
+          options.attribute = 'objects';
+          console.log('Set the options.attribute option to save it as something other than objects.');
+        }
+
+      }
+      try {
+      cv.readImage(this.paperclip.file().file.buffer, function(err, im){
+        im.detectObject(options.dataset, {}, function(err, objects){
+          var object = {};
+
+
+          if (objects.length > 0 || options.presence) {
+
+            object[options.attribute] = { count: objects.length, data: objects };
+
+            if (objects.length > 0 && options.tag) {
+              object.object_tags = [options.attribute]
+            }
+
+            next(err, object);
+
+          } else {
+
+            next(err, {});
+
+          }
+
+        });
+      })
+      } catch (err) {
+        console.log(err);
+        next(err, {});
+      }
   }
 
   return obj;
